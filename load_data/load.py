aaf38eb16826a94c6c98b8655e484bc31f4232d8
herve <dockerlite@gmail.com>
2019-07-30 07:01:05 +0200

    first commit

---
+++ load_data/load.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import re
+
+path='/tmp'
+
+files= [f for f in glob.glob(path+"**/*.dat")]
+
+for f in files:
+  m= re.search('(\/tmp\/)(.*)(....)', f)
+  table=m.group(2)
+  #print table
+
+  m = re.search('(\/tmp\/)(.*)', f)
+  data_file=m.group(2)
+  #print data_file
+
+  cmd="sed 's/.$//' {} | tee /tmp/newvalue.dat && mv /tmp/newvalue.dat {} ".format(f,f)
+  os.system(cmd)
+
+  print("TRUNCATE table...",table)
+  cmd= "psql test-tpc -q -c 'TRUNCATE {}'".format(table)
+  os.system(cmd )
+
+  print("Loading  data file...",f )
+  cmd= """psql test-tpc -c "copy {} FROM '{}' CSV DELIMITER '|' "  """.format(table, f)
+  os.system(cmd )
+
+
+ #  #psql tpcds -q -c "TRUNCATE $table"
+ #  #psql tpcds -c "\\copy $table FROM '$i' CSV DELIMITER '|'"
+
