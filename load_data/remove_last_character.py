520e134c556c9104cf25b35b7a58dc3bd20cac2f
herve <dockerlite@gmail.com>
2019-07-30 10:17:38 +0200

    add remove last character

--- load_data/remove_last_character.py
+++ load_data/remove_last_character.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
-import glob
+import fnmatch
 import os
 import re
 
-path='/tmp'
 
-files= [f for f in glob.glob(path+"**/*.dat")]
+def recursive_glob(rootdir='.', pattern='*'):
+    return [os.path.join(looproot, filename)
+            for looproot, _, filenames in os.walk(rootdir)
+            for filename in filenames
+            if fnmatch.fnmatch(filename, pattern)]
 
-for f in files:
-  m= re.search('(\/tmp\/)(.*)(....)', f)
-  table=m.group(2)
-  #print table
 
-  m = re.search('(\/tmp\/)(.*)', f)
-  data_file=m.group(2)
-  #print data_file
+datfiles = recursive_glob('/tmp', '*.dat')
 
-  cmd="sed 's/.$//' {} | tee /tmp/newvalue.dat && mv /tmp/newvalue.dat {} ".format(f,f)
-  os.system(cmd)
+for f in datfiles:
+    m = re.search('(\/tmp\/)(.*)(....)', f)
+    table = m.group(2)
+    # print table
+
+    m = re.search('(\/tmp\/)(.*)', f)
+    data_file = m.group(2)
+    # print data_file
+
+    cmd = "sed 's/.$//' {} | tee /tmp/newvalue.dat && mv /tmp/newvalue.dat {} ".format(f, f)
+    os.system(cmd)
