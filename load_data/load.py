30a9d48bf98787a8b9eeea35479adeb92329b794
Herve <dockerlite@gmail.com>
2020-05-02 08:02:17 +0200

    changed to pgloader

--- load_data/load.py
+++ load_data/load.py
@@ -2,25 +2,10 @@
 
 import glob
 import os
-import re
 
-
-
-files= [f for f in glob.glob("*.ctl")]
+files= glob.glob("*.ctl")
 
 for f in files:
-  m= re.search('(\/tmp\/)(.*)(....)', f)
-  table=m.group(2)
-  #print table
-
-  m = re.search('(\/tmp\/)(.*)', f)
-  data_file=m.group(2)
-  #print data_file
-
-  #print("TRUNCATE table...",table)
-  #cmd= "psql test-tpc -q -c 'TRUNCATE {}'".format(table)
-  #os.system(cmd )
-
   print("Loading  data file...",f )
   cmd= "pgloader {}".format(f)
   os.system(cmd )
